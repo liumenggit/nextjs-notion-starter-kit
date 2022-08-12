e1a9dca709da458f6697671e2d7432e87c2b310e
刘老六 <admin@autoahk.com>
2022-08-12 13:08:37 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd16270a630ca4a58be19753be9dbb805',
+  rootNotionPageId: '2261b6de96814a96803bfa7e7ffda3d3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
